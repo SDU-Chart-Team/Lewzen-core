233527037fd221df17e77f12ab29100301969940
poi0qwe <1409647683@qq.com>
2022-03-23 00:13:02 +0800

    22.03.23

---
+++ core/svg/svg_define.h
@@ -0,0 +1,18 @@
+#include <string>
+#ifndef __LZ_SVG_DEFINE__
+#define __LZ_SVG_DEFINE__
+
+namespace Lewzen {
+    class SVGDefine {
+
+    }
+
+    class FillStyle: public SVGDefine {
+
+    }
+
+    class StrokeStyle: public SVGDefine {
+        
+    }
+}
+#endif
---
+++ core/svg/svg_element.h
@@ -0,0 +1,10 @@
+#include <string>
+#ifndef __LZ_SVG_ELEMENT__
+#define __LZ_SVG_ELEMENT__
+
+namespace Lewzen {
+    class SVGElement {
+
+    }
+}
+#endif
